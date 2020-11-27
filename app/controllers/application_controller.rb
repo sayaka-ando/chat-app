a1d309fc683c09a21447323e1bca12cd12d475ca
sayaka-ando <sayayann82@yahoo.co.jp>
2020-11-27 11:44:19 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # →ログイン状態に応じて対応してくれる。未ログインユーザーはログインページに戻せる
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # 「特定のカラムを許容する」メソッド。今回は「nameカラム」を追加したので「name」キーの内容の保存をpermitメソッドで許可
+  end
 end
